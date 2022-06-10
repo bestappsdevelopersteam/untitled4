72965729e24dbda90ad6972b239d96584c82ffd1
bestappsdevelopersteam <bestappsdevelopersteam@gmail.com>
2022-06-10 20:05:45 +0300

    My first project for two arrays.

---
+++ src/Rec.java
@@ -0,0 +1,23 @@
+import static java.lang.System.*;
+
+public class Rec {
+
+    public static void main(String[] args) {
+
+        out.println("1. Lesson Rectangle 3*5 ");
+        out.println("*****");
+        out.println("*****");
+        out.println("*****");
+
+        out.println("--------------------------- ");
+        out.println("2. Lesson Square 3*5 ");
+        out.println("_____");
+        out.println("|    |");
+        out.println("|    |");
+        out.println("|    |");
+        out.println("_____");
+
+        out.println("--------------------------- ");
+        out.println("created by Plamen  on 20.04.22");
+    }
+}
